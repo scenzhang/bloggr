f2ae500b34b11b8cac08f974944326bb12b9586f
Sam Cenzhang <s.cenzhang@gmail.com>
2018-02-27 23:09:02 -0500

    add index, show, new routes/views

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
+const fs = require('fs')
 // app.get('/', (req, res) => res.end('hello world'))
 
 app.listen(3000, () => console.log('Listening on port 3000'))
@@ -11,7 +11,26 @@ app.set('view engine', 'pug')
 
 app.use(express.static(path.join(__dirname, 'static')));
 
-app.get('/', (req, res) => res.render('hello', { body: "asdf"}, (err, html) => {
-  console.log(html);
-  res.send(html);
-}))
+const blogFile = fs.readFileSync('./seeds/blogs.json', 'utf-8')
+const blogArray = JSON.parse(blogFile)
+const blogs = {}
+blogArray.forEach((blog) => {
+  blogs[blog._id] = blog;
+});
+
+app.get('/hello', (req, res) => res.render('hello'))
+app.get('/', (req, res) =>
+  res.render('index', {
+    blogs: blogArray
+  })
+)
+
+app.get('/blogs/:id', (req, res) => {
+  res.render('show', {
+    blog: blogs[req.params.id]
+  })
+})
+
+app.get('/new', (req, res) => {
+  res.render('new');
+})
