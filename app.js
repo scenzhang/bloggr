15ca70b116808e96ba856d05824be7f8f01a0d10
Sam Cenzhang <s.cenzhang@gmail.com>
2018-02-28 23:10:55 -0500

    add edit, delete routes

--- app.js
+++ app.js
@@ -1,48 +1,65 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const fs = require('fs')
+const fs = require('fs');
 //parse forms
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 //generate ids for new posts to write in file
-const uuid = require('uuid/v1')
+const uuid = require('uuid/v1');
+//put and delete for forms
+const methodOverride = require('method-override');
 
 const urlencodedParser = bodyParser.urlencoded({
   extended: false
-})
+});
 
-app.listen(3000, () => console.log('Listening on port 3000'))
+app.listen(3000, () => console.log('Listening on port 3000'));
 
-app.set('views', 'views')
-app.set('view engine', 'pug')
+app.set('views', 'views');
+app.set('view engine', 'pug');
 
 app.use(express.static(path.join(__dirname, 'static')));
 
-const blogFile = fs.readFileSync('./seeds/blogs.json', 'utf-8')
-const blogArray = JSON.parse(blogFile)
-const blogs = {}
-blogArray.forEach((blog) => {
-  blogs[blog._id] = blog;
-});
+app.use(methodOverride('_method'));
 
-app.get('/hello', (req, res) => res.render('hello'))
-app.get('/', (req, res) =>
+
+
+app.get('/hello', (req, res) => res.render('hello'));
+app.get('/', (req, res) => {
+  const blogFile = fs.readFileSync('./seeds/blogs.json', 'utf-8')
+  const blogArray = JSON.parse(blogFile)
+  const blogs = {}
+  blogArray.forEach((blog) => {
+    blogs[blog._id] = blog;
+  });
+
   res.render('index', {
     blogs: blogArray
-  })
-)
+  });
 
-app.get('/blogs/:id', (req, res) => {
+});
+
+app.get('/:id', (req, res) => {
+  const blogFile = fs.readFileSync('./seeds/blogs.json', 'utf-8')
+  const blogArray = JSON.parse(blogFile)
+  const blogs = {}
+  blogArray.forEach((blog) => {
+    blogs[blog._id] = blog;
+  });
+
   res.render('show', {
     blog: blogs[req.params.id]
-  })
-})
+  });
+});
 
 app.get('/new', (req, res) => {
   res.render('new');
-})
+});
 
 app.post('/', urlencodedParser, (req, res) => {
+  const blogFile = fs.readFileSync('./seeds/blogs.json', 'utf-8')
+  const blogArray = JSON.parse(blogFile)
+
   
   const newBlog = {
     author: req.body.author || 'anon',
@@ -55,4 +72,63 @@ app.post('/', urlencodedParser, (req, res) => {
   blogArray.push(newBlog);
   fs.writeFileSync('./seeds/blogs.json', JSON.stringify(blogArray, null, 2));
   res.redirect('/');
-})
+});
+
+app.get('/:blogId/edit', (req, res) => {
+  const blogId = req.params.blogId;
+  const blogArray = JSON.parse(fs.readFileSync('./seeds/blogs.json', 'utf-8'));
+  let blog;
+  for (let i = 0; i < blogArray.length; i++) {
+    if (blogArray[i]._id === blogId) {
+      blog = blogArray[i];
+      break;
+    }
+  }
+  if (blog !== undefined) {
+    res.render('edit', {
+      blog
+    });
+  } else {
+    res.status(404).end('Blog Not Found');
+  }
+});
+
+app.delete('/:blogId', (req, res) => {
+  const blogId = req.params.blogId
+  const blogArray = JSON.parse(fs.readFileSync('./seeds/blogs.json', 'utf-8'))
+  const newBlogArray = blogArray.filter(b => b._id !== blogId)
+
+  fs.writeFileSync('./seeds/blogs.json', JSON.stringify(newBlogArray, null, 2))
+  res.redirect('/');
+});
+
+app.put('/:blogId', urlencodedParser, (req, res) => {
+  const blogId = req.params.blogId;
+  const blogArray = JSON.parse(fs.readFileSync('./seeds/blogs.json', 'utf-8'));
+  let blog, blogIdx;
+  for (let i = 0; i < blogArray.length; i++) {
+    if (blogArray[i]._id === blogId) {
+      blog = blogArray[i];
+      blogIdx = i;
+      break;
+    }
+  }
+  if (blog !== undefined) {
+    const {
+      author,
+      title,
+      blog_body: body
+    } = req.body;
+    const updatedBlog = Object.assign({}, blog, {
+      author,
+      title,
+      body,
+      updatedAt: Date.now()
+    });
+    blogArray[blogIdx] = updatedBlog;
+    fs.writeFileSync('./seeds/blogs.json', JSON.stringify(blogArray, null, 2));
+    res.redirect(`/${blogId}`);
+  } else {
+    res.status(404).end('Blog Not Found');
+  }
+});
